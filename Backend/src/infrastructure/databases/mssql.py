85e08df97f473cd6a5c12e7ea3f18de5dcdc0506
tranminhhung2006 <hungtm1944@ut.edu.vn>
2025-09-06 08:15:18 +0700

    mssql

--- Backend/src/infrastructure/databases/mssql.py
+++ Backend/src/infrastructure/databases/mssql.py
@@ -2,11 +2,19 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from config import Config
 from infrastructure.databases.base import Base
+from sqlalchemy.orm import sessionmaker, scoped_session
 
 # Database configuration
 DATABASE_URI = Config.DATABASE_URI
 engine = create_engine(DATABASE_URI)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-session = SessionLocal()
+
+session_factory = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+session = scoped_session(session_factory)
 def init_mssql(app):
     Base.metadata.create_all(bind=engine)
